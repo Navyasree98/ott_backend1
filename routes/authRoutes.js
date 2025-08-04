738f5586822df84a2d64f4e4b6aa80b08fa7fece
Navya Santhapeta <navya.santhapeta@kellton.com>
2025-08-04 16:00:01 -0400

    created endpoint for logout.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require('express'); 
 const router = express.Router();
-const { register, login } = require('../controllers/authcontroller');
 
+const { register, login, logout } = require('../controllers/authcontroller.js');
+const verifyToken = require('../middleware/auth');
+
+// use the destructured function names directly
 router.post('/register', register);
 router.post('/login', login);
-
-
+router.post('/logout', verifyToken, logout); // Add middleware here to extract token info
 
 module.exports = router;
